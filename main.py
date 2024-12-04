73d7e3e95af532b60ac0eb8bf01dfa8c43529253
EgorovAleksei <plyuh@list.ru>
2024-12-04 11:22:09 +0300

    istall sqlalchemy 2.0.36

--- main.py
+++ main.py
@@ -8,24 +8,16 @@ app.include_router(items_router)
 app.include_router(users_router)
 
 
-
 @app.get("/hello")
 def hello(name: str = "World"):
     name = name.strip().title()
     return {"message": f"Hello, {name}!!!1122"}
 
 
-
 @app.post("/calc/add")
 def add(a: int, b: int):
-    return {
-        "a": a,
-        "b": b,
-        "result": a + b
-    }
-
-
+    return {"a": a, "b": b, "result": a + b}
 
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run("main:app", reload=True)
--- users/crud.py
+++ users/crud.py
@@ -1,8 +1,8 @@
 from users.schemas import CreateUser
 
+
 def create_user(user_in: CreateUser):
     user = user_in.model_dump()
-    print(user)
     return {
         "success": True,
         "user": user,
